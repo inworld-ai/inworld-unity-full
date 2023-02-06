2167eb89abcb6a25834983ffb29e983610f98aae
Div-Inworld <divakar@inworld.ai>
2023-02-06 12:45:49 -0800

    Proper license format without added comment

--- Inworld.AI/Scripts/Runtime/2DInteraction/Stone.cs
+++ Inworld.AI/Scripts/Runtime/2DInteraction/Stone.cs
@@ -1,11 +1,10 @@
-/*
- * /*************************************************************************************************
- * * Copyright 2022 Theai, Inc. (DBA Inworld)
- * *
- * * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
- * * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
- * *************************************************************************************************/
- */
+/*************************************************************************************************
+* Copyright 2022 Theai, Inc. (DBA Inworld)
+*
+* Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+* that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+*************************************************************************************************/
+
 
 ﻿using UnityEngine;
 using UnityEngine.Tilemaps;
