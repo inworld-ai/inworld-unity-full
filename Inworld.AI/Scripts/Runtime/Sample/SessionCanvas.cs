c8b246d3bc3ecf27f2596fc3f79f02753729cc6e
Div-Inworld <divakar@inworld.ai>
2023-02-06 09:06:25 -0800

    Addition of missing licenses to confirm workflow is working

--- Inworld.AI/Scripts/Runtime/Sample/SessionCanvas.cs
+++ Inworld.AI/Scripts/Runtime/Sample/SessionCanvas.cs
@@ -1,3 +1,12 @@
+/*
+ * /*************************************************************************************************
+ * * Copyright 2022 Theai, Inc. (DBA Inworld)
+ * *
+ * * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ * *************************************************************************************************/
+ */
+
 using Inworld.Util;
 using System.Collections;
 using System.Collections.Generic;
