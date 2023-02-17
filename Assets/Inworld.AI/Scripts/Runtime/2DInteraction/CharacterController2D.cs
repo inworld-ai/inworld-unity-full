0fdc616b294876aa0cbb8721eb80cd1928ba48a9
Divakar Dev <divakar@inworld.ai>
2023-02-17 14:36:40 -0800

    Missing licenses

--- Assets/Inworld.AI/Scripts/Runtime/2DInteraction/CharacterController2D.cs
+++ Assets/Inworld.AI/Scripts/Runtime/2DInteraction/CharacterController2D.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿/*************************************************************************************************
+* Copyright 2022 Theai, Inc. (DBA Inworld)
+*
+* Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+* that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+*************************************************************************************************/
+using UnityEngine;
 namespace Inworld.Sample.Interaction2D
 {
     public class CharacterController2D : Controller2D
