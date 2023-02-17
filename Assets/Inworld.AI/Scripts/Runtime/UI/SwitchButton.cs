42bcaf0623dfd8d077725df1fe7cfafd21b2a589
Div-Inworld <123405577+Div-Inworld@users.noreply.github.com>
2023-02-17 14:40:31 -0800

    Missing licenses (#28)

--- Assets/Inworld.AI/Scripts/Runtime/UI/SwitchButton.cs
+++ Assets/Inworld.AI/Scripts/Runtime/UI/SwitchButton.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿/*************************************************************************************************
+* Copyright 2022 Theai, Inc. (DBA Inworld)
+*
+* Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+* that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+*************************************************************************************************/
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Inworld.Runtime
