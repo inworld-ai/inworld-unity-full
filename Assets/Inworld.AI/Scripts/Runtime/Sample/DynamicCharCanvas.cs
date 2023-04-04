7127c0dd7e331a84fc66420154b248e596260a0e
Yan Jin <68676221+Juniverse@users.noreply.github.com>
2023-04-03 18:09:25 -0700

    Update Assets/Inworld.AI/Scripts/Runtime/Sample/DynamicCharCanvas.cs
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Assets/Inworld.AI/Scripts/Runtime/Sample/DynamicCharCanvas.cs
+++ Assets/Inworld.AI/Scripts/Runtime/Sample/DynamicCharCanvas.cs
@@ -1,4 +1,13 @@
-﻿using Inworld.Util;
+/*
+ * /*************************************************************************************************
+ * * Copyright 2022 Theai, Inc. (DBA Inworld)
+ * *
+ * * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ * *************************************************************************************************/
+ */
+
+using Inworld.Util;
 using UnityEngine;
 namespace Inworld.Sample
 {
