aa402815bd05bdd0094940e2c8a231559eaa60da
Yan Jin <601746725@qq.com>
2023-07-27 10:57:07 -0700

    Add header

--- Assets/Inworld.AI/Scripts/Runtime/3DInteraction/InworldInnequinAnimation.cs
+++ Assets/Inworld.AI/Scripts/Runtime/3DInteraction/InworldInnequinAnimation.cs
@@ -1,4 +1,10 @@
-﻿using Inworld.Audio;
+﻿/*************************************************************************************************
+* Copyright 2022 Theai, Inc. (DBA Inworld)
+*
+* Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+* that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+*************************************************************************************************/
+using Inworld.Audio;
 using Inworld.Grpc;
 using Inworld.Util;
 using System.Collections.Generic;
