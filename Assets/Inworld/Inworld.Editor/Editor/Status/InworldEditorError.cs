86203989eb29bc230c5ecce4c92b21ad4d749d96
Yan Jin <601746725@qq.com>
2023-08-29 16:13:14 -0700

    Add Error processing

--- Assets/Inworld/Inworld.Editor/Editor/Status/InworldEditorError.cs
+++ Assets/Inworld/Inworld.Editor/Editor/Status/InworldEditorError.cs
@@ -1,11 +1,15 @@
-﻿namespace Inworld.AI.Editor
+﻿using UnityEditor;
+using UnityEngine;
+namespace Inworld.AI.Editor
 {
     public class InworldEditorError : IEditorState
     {
 
         public void DrawTitle()
         {
-            
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField(InworldEditor.Instance.Error, InworldEditor.Instance.TitleStyle);
+            EditorGUILayout.Space();
         }
         public void DrawContent()
         {
@@ -13,7 +17,11 @@
         }
         public void DrawButtons()
         {
-
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Back", InworldEditor.Instance.BtnStyle))
+            {
+                InworldEditor.Instance.Status = EditorStatus.Init;
+            }
         }
         public void OnExit()
         {
