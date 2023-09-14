62d00e1619c93375743b9b04bbc092e01b198a7b
Yan Jin <601746725@qq.com>
2023-09-14 14:29:35 -0700

    CORE-3918 add null ref protection

--- Assets/Inworld/Inworld.Samples.RPM/Scripts/PlayerControllerRPM.cs
+++ Assets/Inworld/Inworld.Samples.RPM/Scripts/PlayerControllerRPM.cs
@@ -36,7 +36,11 @@ namespace Inworld.Sample.RPM
             if (m_StatusText)
                 m_StatusText.text = newStatus.ToString();
             if (newStatus == InworldConnectionStatus.Error)
-                m_StatusText.text = InworldController.Client.Error;
+            {
+                if (m_StatusText)
+                    m_StatusText.text = InworldController.Client.Error;
+            }
+                
         }
         protected override void OnCharacterRegistered(InworldCharacterData charData)
         {
