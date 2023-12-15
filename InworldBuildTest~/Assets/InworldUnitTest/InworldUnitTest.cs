2e5e6718af5f34676552a47830e28474f4beda94
Yan Jin <601746725@qq.com>
2023-12-14 17:07:51 -0800

    use unittest based cases

---
+++ InworldBuildTest~/Assets/InworldUnitTest/InworldUnitTest.cs
@@ -0,0 +1,28 @@
+using Inworld;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        Object.Instantiate(InworldAI.ControllerPrefab2D);
+        Assert.AreEqual(true, InworldController.Instance != null);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        Object.Instantiate(InworldAI.ControllerPrefab2D);
+        Assert.AreEqual(true, InworldController.Instance != null);
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
