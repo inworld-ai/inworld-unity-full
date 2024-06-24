a91e8676b718275e9160a5f89817a23856acbc69
Yan Jin <601746725@qq.com>
2024-06-24 14:18:29 -0700

    Add header

--- Assets/Inworld/Inworld.Native/VAD/VADInterop.cs
+++ Assets/Inworld/Inworld.Native/VAD/VADInterop.cs
@@ -1,4 +1,11 @@
-﻿using System.Runtime.InteropServices;
+﻿/*************************************************************************************************
+ * Copyright 2022-2024 Theai, Inc. dba Inworld AI
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ *************************************************************************************************/
+
+using System.Runtime.InteropServices;
 namespace Inworld.Inworld.Native.VAD
 {
     public class VADInterop
