e11c4abe9101231cb72c690d80bebd35faf5f073
Yan Jin <601746725@qq.com>
2024-07-03 14:38:09 -0700

    Finished until CS connection in inworld

--- Assets/MultiDemo/Scripts/InworldNetworkController.cs
+++ Assets/MultiDemo/Scripts/InworldNetworkController.cs
@@ -10,10 +10,21 @@ public class InworldNetworkController : MonoBehaviour
     public GameObject canvas;
     public TMP_InputField address;
     public TMP_InputField port;
+    NetworkManager m_NetworkManager;
 
+    NetworkManager NetworkManager
+    {
+        get
+        {
+            if (!m_NetworkManager)
+            {
+                m_NetworkManager = NetworkManager.Singleton;
+            }
+            return m_NetworkManager;
+        }
+    }
     protected virtual void Awake()
     {
-        m_Transport = GetComponent<UnityTransport>();
         address.text = m_Transport.ConnectionData.Address;
         port.text = m_Transport.ConnectionData.Port.ToString();
         DontDestroyOnLoad(gameObject);
@@ -21,15 +32,34 @@ public class InworldNetworkController : MonoBehaviour
 
     public void StartServer()
     {
-        if (NetworkManager.Singleton.StartServer())
+        if (NetworkManager.StartServer())
+        {
             canvas.SetActive(false);
+        }
+            
     }
     public void StartClient()
     {
         m_Transport.ConnectionData.Address = address.text;
         if (ushort.TryParse(port.text, out ushort data))
             m_Transport.ConnectionData.Port = data;
-        if (NetworkManager.Singleton.StartClient())
+        if (NetworkManager.StartClient())
             canvas.SetActive(false);
+    }
+    void Update()
+    {
+        CheckConnectionStatus();
+    }
+    void CheckConnectionStatus()
+    {
+        if (!NetworkManager)
+            return;
+        if (controller.gameObject.activeSelf)
+            return;
+        if (NetworkManager.IsServer && !NetworkManager.IsClient)
+        {
+            if (NetworkManager.ConnectedClientsIds.Count > 0)
+                controller.gameObject.SetActive(true);
+        }
     }
 }
