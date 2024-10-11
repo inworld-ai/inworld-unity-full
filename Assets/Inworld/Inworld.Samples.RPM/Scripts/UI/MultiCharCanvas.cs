31596ff4ea0d1bc6c5eec34727478881e4af2ff8
Yan Jin <yan.jin@inworld.ai>
2024-10-11 14:43:41 -0700

    Lower the threshold since the character's voice is lower

--- Assets/Inworld/Inworld.Samples.RPM/Scripts/UI/MultiCharCanvas.cs
+++ Assets/Inworld/Inworld.Samples.RPM/Scripts/UI/MultiCharCanvas.cs
@@ -8,6 +8,8 @@
 using UnityEngine;
 using Inworld.Entities;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.InputSystem;
 
 namespace Inworld.Sample.RPM
@@ -24,7 +26,7 @@ namespace Inworld.Sample.RPM
         string m_Instruction = "Press <color=green>\"Tab\"</color> to switch character selection method.\n";
         string m_CurrentMethod;
         string m_CharacterIndicator = k_GroupChat;
-
+        bool m_AutoChatInitiated;
         protected virtual void Awake()
         {
             string switchSelection = m_SwitchSelectionMethod.bindings[0].path;
@@ -50,12 +52,45 @@ namespace Inworld.Sample.RPM
             m_SwitchSelectionMethod.Enable();
             m_AutoChat.Enable();
             m_CurrentMethod = _GetCurrentInstruction();
+            if (InworldController.CharacterHandler)
+            {
+                InworldController.CharacterHandler.Event.onCharacterListJoined.AddListener(OnCharacterListJoined);
+                InworldController.CharacterHandler.Event.onConversationUpdated.AddListener(OnConversationUpdated);
+            }
         }
         protected override void OnDisable()
         {
             base.OnDisable();
             m_SwitchSelectionMethod.Disable();
             m_AutoChat.Disable();
+            if (InworldController.CharacterHandler)
+            {
+                InworldController.CharacterHandler.Event.onCharacterListJoined.RemoveListener(OnCharacterListJoined);
+                InworldController.CharacterHandler.Event.onConversationUpdated.RemoveListener(OnConversationUpdated);
+            }
+        }
+        protected bool IsAutoChatInitiatingValid => !m_AutoChatInitiated &&
+                                              InworldController.Client &&
+                                              InworldController.Client.AutoChat &&
+                                              InworldController.CharacterHandler &&
+                                              InworldController.CharacterHandler.CurrentCharacters != null &&
+                                              InworldController.CharacterHandler.CurrentCharacters.Count >= 2;
+        
+        
+        protected void OnCharacterListJoined(InworldCharacter character)
+        {
+            if (!IsAutoChatInitiatingValid)
+                return;
+            // YAN: Use cached update conversation to start server immediately, if AutoChat is toggled. 
+            InworldController.Client.UpdateConversation(InworldController.CharacterHandler.ConversationID, InworldController.CharacterHandler.CurrentCharacterNames, false);
+        }
+        protected void OnConversationUpdated()
+        {
+            // YAN: If Server started and conversation updated callback received. Immediately send 
+            if (!IsAutoChatInitiatingValid)
+                return;
+            InworldController.CharacterHandler.NextTurn();
+            m_AutoChatInitiated = true;
         }
         void Update()
         {
