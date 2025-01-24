7beb96070f007ae15c466abae0fdd020ea63b68e
Yan Jin <yan.jin@inworld.ai>
2025-01-23 17:26:22 -0800

    INTG-2395 Update license yrs

--- Assets/Inworld/Inworld.Samples.RPM/Scripts/UI/DynamicCharCanvas.cs
+++ Assets/Inworld/Inworld.Samples.RPM/Scripts/UI/DynamicCharCanvas.cs
@@ -1,5 +1,5 @@
 /*************************************************************************************************
-* Copyright 2022-2024 Theai, Inc. dba Inworld AI
+* Copyright 2022-2025 Theai, Inc. dba Inworld AI
 *
 * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
 * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
